7c9781966ca1129fbb4f3ea676d0f9634a8fe473
Doomsday <84427572+D00MSDAYDEVICE@users.noreply.github.com>
2025-04-08 11:26:38 -0300

    Add files via upload

---
+++ factoryvote.py
@@ -0,0 +1,80 @@
+# Copyright (C) 2025 Doomsday
+# This is an extension plugin for minqlx to callvote factories
+# Place the factories you want votable in factories.txt in your /baseq3 folder
+# Originally created for Thunderdome tournament servers
+
+# You can redistribute it and/or modify it under the terms of the
+# GNU General Public License as published by the Free Software Foundation,
+# either version 3 of the License, or (at your option) any later version.
+
+# You should have received a copy of the GNU General Public License
+# along with minqlx. If not, see <http://www.gnu.org/licenses/>.
+
+# Created by Doomsday
+# https://github.com/D00MSDAYDEVICE
+# https://www.youtube.com/@HIT-CLIPS
+
+# You are free to modify this plugin.
+# This plugin comes with no warranty or guarantee.
+
+import minqlx
+import os
+import time
+
+class factoryvote(minqlx.Plugin):
+    def __init__(self):
+        self.add_command("factoryvote", self.cmd_factoryvote, 2)  # Admin level 2
+        self.add_command("fv", self.cmd_factoryvote, 2)           # Shortcut
+
+        self.factories = self.load_factories()
+
+    def load_factories(self):
+        factories_file = os.path.join(self.get_cvar("fs_basepath"), "baseq3", "factories.txt")
+        if not os.path.isfile(factories_file):
+            self.logger.warning("factories.txt not found in baseq3.")
+            return None  # File not found
+        with open(factories_file, "r") as f:
+            factories = [line.strip() for line in f if line.strip()]
+        if not factories:
+            self.logger.warning("factories.txt is empty.")
+        return factories
+
+    def cmd_factoryvote(self, player, msg, channel):
+        if self.factories is None:
+            player.tell("^1Error:^7 factories.txt not found in baseq3.")
+            return
+
+        if not self.factories:
+            player.tell("^1Error:^7 factories.txt is empty.")
+            return
+
+        if len(msg) == 1:
+            player.tell("^3Available Factories:")
+            for idx, factory in enumerate(self.factories, start=1):
+                player.tell("^7{}: {}".format(idx, factory))
+            player.tell("^3Use ^7!fv <number> ^3to start a vote.")
+            return
+
+        try:
+            selection = int(msg[1])
+        except ValueError:
+            player.tell("^1Invalid selection. Use a number from the list.")
+            return
+
+        if selection < 1 or selection > len(self.factories):
+            player.tell("^1Invalid factory number.")
+            return
+
+        factory = self.factories[selection - 1]
+        player.tell("^6You selected factory:^7 {}".format(factory))
+        self.msg("^6Starting vote to load current map with factory:^7 {}".format(factory))
+        self.callvote("qlx !map {} {}".format(self.game.map, factory), "Change map to selected factory")
+
+        minqlx.delay(30, self.check_vote_result, factory)
+
+    def check_vote_result(self, factory):
+        if self.game.vote_passed:
+            self.msg("^2Vote passed! Loading factory:^7 {}".format(factory))
+            self.command("qlx !map {} {}".format(self.game.map, factory))
+        else:
+            self.msg("^1Vote failed for factory:^7 {}".format(factory))
