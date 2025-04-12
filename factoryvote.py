ef8cabfca38a722ef7a6ae7a8ba71d392b4fe60f
Doomsday <84427572+D00MSDAYDEVICE@users.noreply.github.com>
2025-04-11 21:11:16 -0300

    Update factoryvote.py

--- factoryvote.py
+++ factoryvote.py
@@ -17,21 +17,25 @@
 # You are free to modify this plugin.
 # This plugin comes with no warranty or guarantee.
 
+# Copyright (C) 2025 Doomsday
+# This is an extension plugin for minqlx to callvote factories
+
 import minqlx
 import os
 import time
 
 class factoryvote(minqlx.Plugin):
     def __init__(self):
-        self.version = "1.1"  # Set your version number here
-        self.add_command("factoryvote", self.cmd_factoryvote, 3)  # Admin level 3
-        self.add_command("fv", self.cmd_factoryvote, 3)           # Shortcut
-        self.add_command("fvv", self.cmd_version, 3)              # New command for version display
+        self.version = "1.4"
+        self.add_command("factoryvote", self.cmd_factoryvote, 3)
+        self.add_command("fv", self.cmd_factoryvote, 3)
+        self.add_command("fvv", self.cmd_version, 3)
+        self.add_command("factory", self.cmd_factory, 0)
 
         self.factories = self.load_factories()
-        self.selected_factory = None  # Store the chosen factory
-        self.add_hook("game_countdown", self.handle_game_countdown)  # Hook for countdown message
-    
+        self.selected_factory = None
+        self.add_hook("game_countdown", self.handle_game_countdown)
+
     def cmd_version(self, player, msg, channel):
         player.tell("^3FactoryVote Plugin Version:^7 {}".format(self.version))
 
@@ -39,7 +43,7 @@ class factoryvote(minqlx.Plugin):
         factories_file = os.path.join(self.get_cvar("fs_basepath"), "baseq3", "factories.txt")
         if not os.path.isfile(factories_file):
             self.logger.warning("factories.txt not found in baseq3.")
-            return None  # File not found
+            return None
         with open(factories_file, "r") as f:
             factories = [line.strip() for line in f if line.strip()]
         if not factories:
@@ -51,6 +55,12 @@ class factoryvote(minqlx.Plugin):
             self.msg("^3Game starting with factory:^7 {}".format(self.selected_factory))
         else:
             self.msg("^3Game starting. No factory selected. Using default settings.")
+
+    def cmd_factory(self, player, msg, channel):
+        if self.selected_factory:
+            player.tell("^3Current loaded factory:^7 {}".format(self.selected_factory))
+        else:
+            player.tell("^3No factory has been selected yet.")
 
     def cmd_factoryvote(self, player, msg, channel):
         if self.factories is None:
@@ -78,12 +88,16 @@ class factoryvote(minqlx.Plugin):
             player.tell("^1Invalid factory number.")
             return
 
-        self.selected_factory = self.factories[selection - 1]  # Store chosen factory
-        player.tell("^6You selected factory:^7 {}".format(self.selected_factory))
-        self.msg("^6Starting vote to load current map with factory:^7 {}".format(self.selected_factory))
-        self.callvote("qlx !map {} {}".format(self.game.map, self.selected_factory), "Change map to selected factory")
+        self.selected_factory = self.factories[selection - 1]
+        player.tell("^3You selected factory:^7 {}".format(self.selected_factory))
 
-        minqlx.delay(30, self.check_vote_result)
+        # Embed the factory name directly into the vote text
+        vote_text = f"Change map to {self.selected_factory} factory?"
+        vote_command = f"qlx !map {self.game.map} {self.selected_factory}"
+
+        self.callvote(vote_command, vote_text)  # Factory name shown in vote
+
+        minqlx.delay(30)(self.check_vote_result)
 
     def check_vote_result(self):
         if self.game.vote_passed:
